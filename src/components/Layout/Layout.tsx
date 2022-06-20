077f6397657940e4d06276691b5bbd8375031f0c
Selien Yorbandi <selienyorbandi@gmail.com>
2022-06-20 17:17:48 -0300

    SideBar Ui for desktop

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,25 @@
 import { ReactElement } from "react";
 import Header from "./Header/Header";
 import styles from "./Layout.module.css";
+import SideBar from "./SideBar/SideBar";
 import TabBar from "./TabBar/TabBar";
 
 type Props = {
-    children: ReactElement | ReactElement[]
-}
+  children: ReactElement | ReactElement[];
+};
 
-function Layout({children}: Props) {
-  return <div className={styles.Layout}>
-    <Header/>
-    <main>
-      {children}
-    </main>
-    <TabBar/>
-  </div>;
+function Layout({ children }: Props) {
+  return (
+    <div className={styles.Layout}>
+      <SideBar />
+      <div>
+        <Header />
+
+        <main>{children}</main>
+      </div>
+      <TabBar />
+    </div>
+  );
 }
 
 export default Layout;
---
+++ src/components/Layout/SideBar/SideBar.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+import logo from "../../../assets/recipes-app-logo-white.png";
+import styles from "./SideBar.module.css";
+
+function SideBar() {
+  return (
+    <nav className={styles.SideBar}>
+      <img src={logo} />
+      <hr />
+      <ul>
+        <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+        <NavLink to="/">
+          <li>Search</li>
+        </NavLink>
+        <NavLink to="/categories">
+          <li>Categories</li>
+        </NavLink>
+        <NavLink to="/saved">
+          <li>Saved</li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
+}
+
+export default SideBar;
