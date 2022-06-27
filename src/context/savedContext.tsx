270da45b9a0efc45b73771db9e9291bdc8fe42ea
Selien Yorbandi <selienyorbandi@gmail.com>
2022-06-27 02:00:19 -0300

    Saved to LocalStorage

--- src/context/savedContext.tsx
+++ src/context/savedContext.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Recipe from "../pages/Recipe/Recipe";
 
 interface Recipe {
@@ -15,9 +15,17 @@ interface ISavedContext {
   hasChanged: boolean;
 }
 
+const savedFromLocalStorage = () => {
+  return JSON.parse(window.localStorage.getItem("savedRecipes") || "[]");
+};
+
+const saveToLocalStorage = (content : Recipe[]) => {
+  window.localStorage.setItem("savedRecipes", JSON.stringify(content || "[]"));
+};
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const defaultState = {
-  savedRecipes: [],
+  savedRecipes: savedFromLocalStorage(),
   saveRecipe: (recipe: Recipe) => void 0,
   isInSaved: (id: string) => false,
   removeRecipe: (recipe: Recipe) => void 0,
@@ -29,25 +37,29 @@ const SavedContext = React.createContext<ISavedContext>(defaultState);
 export const useSavedContext = () => useContext(SavedContext);
 
 export function SavedContextProvider({ children }: { children: JSX.Element | JSX.Element[] }) {
-  const [savedRecipes, setSavedRecipes] = useState<Recipe[]>([]);
+  const [savedRecipes, setSavedRecipes] = useState<Recipe[]>(savedFromLocalStorage());
   const [hasChanged, setHasChanged] = useState(false);
 
-  function saveRecipe(recipe: Recipe) {
+  useEffect(() => {
+    saveToLocalStorage(savedRecipes);
+  }, [hasChanged]);
+
+  const saveRecipe = (recipe: Recipe) => {
     const newSavedRecipes = [...savedRecipes, recipe];
     setSavedRecipes(newSavedRecipes);
     setHasChanged(!hasChanged);
-  }
+  };
 
-  function isInSaved(id: string): boolean {
+  const isInSaved = (id: string): boolean => {
     for (let i = 0; i < savedRecipes.length; i++) {
       if (savedRecipes[i].idMeal === id) {
         return true;
       }
     }
     return false;
-  }
+  };
 
-  function removeRecipe(recipe: Recipe): void {
+  const removeRecipe = (recipe: Recipe): void => {
     for (let i = 0; i < savedRecipes.length; i++) {
       if (recipe.idMeal === savedRecipes[i].idMeal) {
         savedRecipes.splice(i, 1);
@@ -55,7 +67,7 @@ export function SavedContextProvider({ children }: { children: JSX.Element | JSX
         break;
       }
     }
-  }
+  };
 
   return (
     <SavedContext.Provider
